db094aa7c04b274a1a69e5f9a66b7d63844388c7
Devinlu45 <128208070+Devinlu45@users.noreply.github.com>
2024-02-29 18:52:59 -0800

    Add files via upload

--- src/routes/tables/[name]/+page.server.ts
+++ src/routes/tables/[name]/+page.server.ts
@@ -1,6 +1,43 @@
 import { getRawTableData } from '$lib/database';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { add_data } from '$lib/database';
+import { delete_data } from '$lib/database';
+import { patch_data } from '$lib/database';
+import { table } from 'console';
+
+export const actions = {
+	async post({params, request}) {
+		const tableName = params.name;
+		const data = await request.formData();
+		const MovieID = data.get('Movie_ID');
+		const MovieName = data.get('Name');
+		const Run_Time = data.get('Run_Time');
+		const Urls = data.get('URLs');
+		let result = await add_data('Movies', MovieID, MovieName, Run_Time, Urls)
+	},
+	async delete({ params, request }) {
+		console.log("This is running delete")
+		const tableName = params.name;
+		const data = await request.formData();
+		const ID = data.get('id');
+		console.log(ID)
+		let result = await delete_data(tableName, ID)
+	},
+	async patch({ params, request}){
+		console.log("This is running patch")
+		const tableName = params.name;
+		const data = await request.formData();
+		const MovieID = data.get('Movie_ID');
+		const MovieName = data.get('Name');
+		const Run_Time = data.get('Run_Time');
+		const Urls = data.get('URLs');
+		let result = await patch_data(tableName, MovieID, MovieName, Run_Time, Urls)
+	},
+
+}
+
+
 
 export const load = (async ({ params }) => {
 	const tableName = params.name;
